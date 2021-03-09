eaeac317b9d1d701173cc5246e323ef3eff16bfc
Kseniya Barkova <kcancer17@gmail.com>
2021-03-09 16:54:25 +0200

    feat: add the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let brackets = "[]{}()<>||",
+  		emptyArr = [];
+
+  	for(let bracket of str) {
+    	let bracketsIndex = brackets.indexOf(bracket)
+
+    if (bracketsIndex === -1){
+      continue
+    }
+
+    if(bracketsIndex % 2 === 0) {
+      emptyArr.push(bracketsIndex + 1)
+    } else {
+      if(emptyArr.pop() !== bracketsIndex) {
+        return false;
+      }
+    }
+  }
+    return emptyArr.length === 0
 }
